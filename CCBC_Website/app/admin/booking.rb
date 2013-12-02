c98a4def5d92c51887c20814b770f6a3d3ec3661
Simon <simon@chetwood.org.uk>
2013-12-02 03:43:08 +0000

    Fixed not being able to change date values in Booking, set the colours for the text in the lawns and altered the text linking to the table updater

--- CCBC_Website/app/admin/booking.rb
+++ CCBC_Website/app/admin/booking.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Booking do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :id, :lawn1, :lawn2, :lawn3, :date
+  permit_params :lawn1, :lawn2, :lawn3, :day
   #
   # or
   #
